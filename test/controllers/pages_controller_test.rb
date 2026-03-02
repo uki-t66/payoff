d896ad5043524c4b66a0029db93ec73ab7f8d27b
uki-t66 <trupp55nuu@gmail.com>
2026-03-02 14:31:03 +0900

    fix: テスト内のルーティング修正とGemfile内のシングルクオーテーションをダブルクォーテーションへ修正

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
-    get pages_top_url
+    get root_url
     assert_response :success
   end
 end
