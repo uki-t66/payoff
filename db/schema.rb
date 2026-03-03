2cbf8c349f89b0321b7026eae0c9e764f9c0375d
uki-t66 <trupp55nuu@gmail.com>
2026-03-03 21:13:12 +0900

    feat: categoriesテーブルのマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_02_140638) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_03_121024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name", null: false
+    t.string "color"
+    t.boolean "is_preset", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
