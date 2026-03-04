113bd921467fd65067bdf49943dd3e31149d865d
uki-t66 <trupp55nuu@gmail.com>
2026-03-04 19:36:05 +0900

    feat: ログイン・ログアウト用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   get "dashboard", to: "dashboard#index"
   get "dashboard/index"
   get "users/new"
+  get    "login",  to: "sessions#new"
+  post   "login",  to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
