502f6b15bd6a0ab4629a3d55309647897baaae53
uki-t66 <trupp55nuu@gmail.com>
2026-03-08 21:32:42 +0900

    feat: FixedExpensesControllerにeditとupdateをuuid対応で実装

--- app/controllers/fixed_expenses_controller.rb
+++ app/controllers/fixed_expenses_controller.rb
@@ -1,5 +1,6 @@
 class FixedExpensesController < ApplicationController
   before_action :require_login
+  before_action :set_fixed_expense, only: [ :edit, :update ]
 
   def index
     @fixed_expenses = current_user.fixed_expenses
@@ -23,7 +24,24 @@ class FixedExpensesController < ApplicationController
     end
   end
 
+  def edit
+    @categories = Category.where(is_preset: true)
+  end
+
+  def update
+    if @fixed_expense.update(fixed_expense_params)
+      redirect_to fixed_expenses_path, notice: "固定費を更新しました"
+    else
+      @categories = Category.where(is_preset: true)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_fixed_expense
+    @fixed_expense = current_user.fixed_expenses.find_by!(uuid: params[:uuid])
+  end
 
   def fixed_expense_params
     params.require(:fixed_expense).permit(:name, :category_id, :fixed_cost_amount, :payment_day)
