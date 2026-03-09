a1c6635890bb2cdfba62bb5fa281d33dcb135874
uki-t66 <trupp55nuu@gmail.com>
2026-03-09 18:36:52 +0900

    feat: DashboardControllerにDashboardCalculatorを組み込む

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,9 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def index
+    @calc = DashboardCalculator.new(current_user)
+    @fixed_expenses = current_user.fixed_expenses
+                                  .includes(:category)
+                                  .order(:payment_day)
   end
 end
